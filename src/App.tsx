0f508d07fcabe7a0cad35b9c77fad482cc662b3e
k2 <izmail.k2@gmail.com>
2025-01-17 14:04:07 +0300

    style: integrated custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import * as s from './App.module.scss'
 
 const App = () => {
-  return <h1>Привет, Коля! 🚀</h1>;
+  return <h1 className={s.app}>Привет, Коля! 🚀</h1>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -22,22 +22,41 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.scss$/,
+        test: /\.module\.scss$/, // для модульных стилей
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.scss$/, // для обычных стилей
+        exclude: /\.module\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
   ],
 };
-
