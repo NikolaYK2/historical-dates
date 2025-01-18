db0b7f44e1a93461295ec8f67fb9410cfa6d3bad
k2 <izmail.k2@gmail.com>
2025-01-18 15:06:44 +0300

    feat: use CircularNav

--- src/feachers/ui/main/Main.tsx
+++ src/feachers/ui/main/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as s from "./Main.module.scss";
 import { Typography } from "@/common/componnents/typography/Typography";
+import { CircularNav } from "@/feachers/ui/main/circularNav/CircularNav";
 
 export const Main = () => {
   return (
@@ -9,6 +10,7 @@ export const Main = () => {
         <Typography className={s.h1} variant={"h1"}>
           Исторические даты
         </Typography>
+        <CircularNav />
       </section>
     </main>
   );
