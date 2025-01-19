60f549684167360d08a85ebb241889e3f51d8175
k2 <izmail.k2@gmail.com>
2025-01-19 14:58:34 +0300

    refactor: use SpinnerNav

--- src/feachers/ui/main/Main.tsx
+++ src/feachers/ui/main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as s from "./Main.module.scss";
 import { Typography } from "@/common/componnents/typography/Typography";
-import { CircularNav } from "@/feachers/ui/main/circularNav/CircularNav";
+import { SpinnerNav } from "@/feachers/ui/main/spinnerNav/SpinnerNav";
 
 export const Main = () => {
   return (
@@ -10,7 +10,7 @@ export const Main = () => {
         <Typography className={s.h1} variant={"h1"}>
           Исторические даты
         </Typography>
-        <CircularNav />
+        <SpinnerNav />
       </section>
     </main>
   );
