a2c6d6b6df052f633c7ef4943715e398bc3a3be9
k2 <izmail.k2@gmail.com>
2025-01-20 09:53:27 +0300

    style: add class span

--- src/common/componnents/typography/Typography.tsx
+++ src/common/componnents/typography/Typography.tsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import {ComponentPropsWithoutRef, ElementType} from "react";
-import * as s from './Typography.module.scss'
-import {clsx} from "clsx";
+import React from "react";
+import { ComponentPropsWithoutRef, ElementType } from "react";
+import * as s from "./Typography.module.scss";
+import { clsx } from "clsx";
 
-type VariantType = 'h1' | "h2" | "h3" | 'pReg' | 'pBold';
+type VariantType = "h1" | "h2" | "h3" | "pReg" | "pBold";
 
-type TypographyProps<T extends ElementType = 'p'> = {
+type TypographyProps<T extends ElementType = "p"> = {
   variant?: VariantType;
   className?: string;
 } & ComponentPropsWithoutRef<T>;
 
-export const Typography = <T extends ElementType = 'p'>(props: TypographyProps<T>) => {
-  const {variant = 'p', className, ...restProps} = props
+export const Typography = <T extends ElementType = "p">(props: TypographyProps<T>) => {
+  const { variant = "p", className, ...restProps } = props;
   const Component = variant && getComponent(variant as VariantType);
 
-  return <Component className={clsx(s[variant], className)} {...restProps} />;
+  return <Component className={clsx(s.span, s[variant], className)} {...restProps} />;
 };
 
 function getComponent(variant: VariantType) {
   switch (variant) {
-    case 'h1': {
-      return 'h1'
+    case "h1": {
+      return "h1";
     }
-    case 'h2': {
-      return 'h2'
+    case "h2": {
+      return "h2";
     }
-    case 'h3': {
-      return 'h3'
+    case "h3": {
+      return "h3";
     }
-    case 'pReg':
-    case 'pBold': {
-      return 'p'
+    case "pReg":
+    case "pBold": {
+      return "p";
     }
     default: {
-      return 'span'
+      return "span";
     }
   }
 }
