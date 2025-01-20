5d02367a22a1ab414ad9407eef340320178353f4
k2 <izmail.k2@gmail.com>
2025-01-20 15:23:24 +0300

    feat: loading fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import {Main} from "@/feachers/ui/main/Main";
+import React, { useEffect, useState } from "react";
+import { Main } from "@/feachers/ui/main/Main";
 
 const App = () => {
-  return <Main/>;
+  const [isFontsLoaded, setIsFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Promise.all([document.fonts.load("bold 1rem PT Sans"), document.fonts.load("bold 1rem PT Sans")]);
+        setIsFontsLoaded(true);
+      } catch (error) {
+        console.error("Ошибка загрузки шрифтов:", error);
+        setIsFontsLoaded(true);
+      }
+    };
+
+    loadFonts();
+  }, []);
+
+  return <>{isFontsLoaded ? <Main /> : <div>Loading...</div>}</>;
 };
 
 export default App;
